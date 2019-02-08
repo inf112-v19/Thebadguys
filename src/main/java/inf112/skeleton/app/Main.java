089cf61c61c216e01b2fe241c54ccff66e933b59
Nominoa <40321508+Nominoa@users.noreply.github.com>
2019-02-08 13:55:44 +0100

    Update Main.java

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -7,9 +7,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "hello-world";
-        cfg.width = 480;
-        cfg.height = 320;
+        cfg.title = "Robo Rally";
+        cfg.width = 640;
+        cfg.height = 480;
+
 
         new LwjglApplication(new HelloWorld(), cfg);
     }
