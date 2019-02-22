78863f1b69ea023ba8a334469bb8b2877577dc71
Tord Stangeland <gus008@uib.no>
2019-02-22 14:38:46 +0100

    Final changes

---
+++ Deliverables/Code for Mandatory2/src/main/java/inf112/skeleton/app/RoboRallyDemo.java
@@ -0,0 +1,143 @@
+package inf112.skeleton.app;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Buttons;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class RoboRallyDemo implements ApplicationListener, InputProcessor {
+    TiledMap tiledMap;
+    TiledMapRenderer tiledMapRenderer;
+    OrthographicCamera camera;
+
+
+    private SpriteBatch batch;
+    private Texture texture;
+    private Sprite sprite;
+    private float posX, posY;
+
+    @Override
+    public void create() {
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+        batch = new SpriteBatch();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, w * 4  ,h * 4);
+        camera.update();
+
+        tiledMap = new TmxMapLoader().load("Models/roborallymap.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+        texture = new Texture(Gdx.files.internal("Models/tank.png"));
+        sprite = new Sprite(texture);
+        posX = -4;
+        posY = 6;
+        sprite.setPosition(posX,posY);
+
+        Gdx.input.setInputProcessor(this);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        texture.dispose();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+
+        sprite.setPosition(posX,posY);
+        batch.begin();
+        sprite.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        float moveAmount = 75.0f;
+        if(Gdx.input.isKeyPressed(Keys.CONTROL_LEFT))
+            moveAmount = 150.0f;
+
+        if(keycode == Keys.LEFT)
+            posX-=moveAmount;
+        if(keycode == Keys.RIGHT)
+            posX+=moveAmount;
+        if(keycode == Keys.UP)
+            posY+=moveAmount;
+        if(keycode == Keys.DOWN)
+            posY-=moveAmount;
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        if(button == Buttons.LEFT){
+            posX = screenX - sprite.getWidth()/2;
+            posY = Gdx.graphics.getHeight() - screenY - sprite.getHeight()/2;
+        }
+        if(button == Buttons.RIGHT){
+            posX = Gdx.graphics.getWidth()/2 - sprite.getWidth()/2;
+            posY = Gdx.graphics.getHeight()/2 - sprite.getHeight()/2;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
