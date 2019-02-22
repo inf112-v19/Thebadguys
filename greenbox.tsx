6f992988167648adfb6d059d1121bcdbe8fd34cf
Tord Stangeland <gus008@uib.no>
2019-02-22 14:10:08 +0100

    Deleting files that have been moved

--- greenbox.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="greenbox" tilewidth="32" tileheight="32" tilecount="81" columns="9">
- <image source="greenbox.png" width="300" height="297"/>
-</tileset>
