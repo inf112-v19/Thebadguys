af00a2c7890448fb04f45ef9c6e313e1dc970a92
Wim <Wim.Broeck@student.uib.no>
2019-03-08 12:19:56 +0100

    Create IRobot interface. Comment IRobot interface and Robot class.

--- src/main/java/inf112/skeleton/app/Robot.java
+++ src/main/java/inf112/skeleton/app/Robot.java
@@ -4,13 +4,16 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
-public class Robot {
+public class Robot implements IRobot{
+
+    // Textures and sprites are now stored initially in the robot class as opposed to only in RoboRallyDemo
     private String textureStr;
     private Texture texture;
     private Sprite sprite;
     private float posX;
     private float posY;
 
+    // Robot constructor
     public Robot(String textureStr, float posX, float posY){
         this.posX=posX;
         this.posY=posY;
@@ -19,6 +22,7 @@ public class Robot {
         this.sprite.setPosition(posX,posY);
     }
 
+    // A constructor that only takes in a sprite and sets its position to some default (0,0)
     public Robot(String textureStr){
         this.posX = 0;
         this.posY = 0;
