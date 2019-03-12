28eae8107290a1bd566f1840645e425a25515904
Wim <Wim.Broeck@student.uib.no>
2019-03-12 14:22:09 +0100

    Not exactly sure whats going on here, I beleive commiting will fix my problem.

---
+++ src/main/java/inf112/skeleton/app/ICard.java
@@ -0,0 +1,25 @@
+package inf112.skeleton.app;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public interface ICard {
+
+    //create the cardSprites in the GUI
+    //public void createCard(float posX, float posY);
+
+    //return the Card Sprite
+    public Sprite getCardSprite();
+
+    //return the Cardslot Sprite
+    //public Sprite getCardSlotSprite();
+
+    public float getPosX();
+
+    public float getPosY();
+
+    public float getDefaultPosX();
+
+    public float getDefaultPosY();
+
+   // public void createCardSlots(float posX, float posY);
+}
