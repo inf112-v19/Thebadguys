f43ba86900d91ed3448479f724d7f1dac489d722
Wim <Wim.Broeck@student.uib.no>
2019-03-28 15:06:49 +0100

    Create GridTest and CardsTest folder. Fix GameMap such that all tests in GridTest work.

--- src/test/java/inf112/skeleton/app/GridTest.java
+++ src/test/java/inf112/skeleton/app/GridTest.java
@@ -11,59 +11,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest{
-
-    Cards testCard = new Cards(10,10, "testNavn", 100);
-    CardSlots testCardSlot = new CardSlots(10,10);
-    Deck deck= new Deck();
-    float test=10;
-
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-
-
-    //trying to make a card and checking that the values are what I expect
-    @Test
-    public void createCardTest(){
-       assertEquals(testCard.getName(), "testNavn");
-       assertEquals(testCard.getPriority(), 100);
-       assertEquals(testCard.getDefaultPosX(), test, 0);
-       assertEquals(testCard.getDefaultPosY(), test, 0);
-       assertEquals(testCard.getDefaultPosY(), testCard.getDefaultPosY(), 0);
-       assertEquals(testCard.getDefaultPosX(), testCard.getDefaultPosX(), 0);
-    }
-
-    //creating a cardslot and checking if the paramaters are what I expect
-    @Test
-    public void createCardSlotTest(){
-        assertEquals(testCardSlot.getPosX(),  test, 0);
-        assertEquals(testCardSlot.getPosY(),  test, 0);
-    }
-
-    //creating a deck, and checking if it creates a empty arrayList
-    @Test
-    public void createDeckTest(){
-        assertEquals(deck.getDeckList().size(), 0);
-    }
-
-    //adding a card to the deck and checking if the size is 1, and if the card is the same as I added.
-    @Test
-    public void addCardToDeckTest(){
-        deck.getDeckList().add(testCard);
-        assertEquals(deck.getDeckList().size(), 1);
-        assertEquals(deck.getDeckList().get(0), testCard);
-    }
-    //trying to put a card into a decklist and check if it exists in the HashMap
-
+public class GridTest {
 
     //creating a grid, and sets an element to a location on that grid, checks if the get method returns same element
     @Test
@@ -133,7 +81,7 @@ public class AppTest{
     }
 
     @Test
-   public void mapGoodMoveTest() throws MovePlayerException{
+    public void mapGoodMoveTest() throws MovePlayerException{
         IGrid grid = new MyGrid(12,12, MapTile.OPEN);
         //sets player position
         grid.set(2,3,MapTile.PLAYER);
@@ -148,8 +96,5 @@ public class AppTest{
         map.movePlayer(Direction.SOUTH);
         assertEquals(MapTile.PLAYER, map.getCell(2, 3));
     }
-
-
-
 
 }
