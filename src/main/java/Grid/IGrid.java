57896976986c6938dd6ef7b832022fb61774b74d
Nominoa <40321508+Nominoa@users.noreply.github.com>
2019-04-09 16:04:07 +0200

    Cleaned up code.

--- src/main/java/Grid/IGrid.java
+++ src/main/java/Grid/IGrid.java
@@ -12,35 +12,12 @@ public interface IGrid<T>{
      */
     int getWidth();
 
-    /**
-     *
-     * Set the contents of the cell in the given x,y location.
-     *
-     * y must be greater than or equal to 0 and less than getHeight().
-     * x must be greater than or equal to 0 and less than getWidth().
-     *
-     * @param x The column of the cell to change the contents of.
-     * @param y The row of the cell to change the contents of.
-     * @param element The contents the cell is to have.
-     */
     void set(int x, int y, T element);
 
     void set(double x, double y, T element);
 
-    /**
-     *
-     * Get the contents of the cell in the given x,y location.
-     *
-     * y must be greater than or equal to 0 and less than getHeight().
-     * x must be greater than or equal to 0 and less than getWidth().
-     *
-     * @param x The column of the cell to get the contents of.
-     * @param y The row of the cell to get contents of.
-     */
     T get(int x, int y);
     T get(double x , double y);
-    T getSpecial(int x, int y);
-    T getSpecial(double x, double y);
 
     /**
      * Make a copy
