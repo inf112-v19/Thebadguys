4a308f2c731b3c615826fb82cf5702a2dcaead93
Andre Vu <andre.vu@student.uib.no>
2019-05-02 16:09:04 +0200

    Merge branch 'Christian' of https://github.com/inf112-v19/Thebadguys into apprentice
    # Conflicts:
    #	Deliverables/Oblig5/SummariesMandatory5.md
    #	src/main/java/inf112/skeleton/app/RoboRallyDemo.java
    #	src/main/java/inf112/skeleton/app/Robot.java

--- src/main/java/inf112/skeleton/app/Robot.java
+++ src/main/java/inf112/skeleton/app/Robot.java
@@ -519,6 +519,7 @@ public class Robot {
                 while (targetHit == false && tempY1 < 12) {
                     if (robot[i].getPosX() == this.getPosX() && robot[i].getPosY() == tempY1) {
                         robot[i].takeDamage();
+                        System.out.println("robot has received damage" + robot[i].getDamage());
                         targetHit = true;
                     } else if (gameMap.getTiles().get((double) this.getPosX(), tempY1 + 0.5) == MapTile.WALL) {
                         targetHit = true;
